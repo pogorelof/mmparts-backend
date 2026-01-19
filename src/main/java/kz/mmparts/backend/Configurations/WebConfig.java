176fb9ad81863fa8aa2548631ec49858a326ba09
Владимир Погорелов <vladimirpogorelov@MacBook-Air-Vladimir.local>
2026-01-19 16:24:08 +0500

    cors

--- src/main/java/kz/mmparts/backend/Configurations/WebConfig.java
+++ src/main/java/kz/mmparts/backend/Configurations/WebConfig.java
@@ -1,6 +1,7 @@
 package kz.mmparts.backend.Configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
